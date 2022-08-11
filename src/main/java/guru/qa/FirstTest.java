01f8deaf9dc8e52f58d2fe82ff1fa40dec3d8154
Mikhail Ishpakhtin <miishp@mail.ru>
2022-08-11 09:41:21 +0300

    Refactoring

--- src/main/java/guru/qa/FirstTest.java
+++ src/main/java/guru/qa/FirstTest.java
@@ -6,11 +6,11 @@ import org.junit.jupiter.api.Test;
 public class FirstTest {
     @Test
     void firstTest(){
-        Assertions.assertFalse(3 < 2);
+        Assertions.assertTrue(3 > 2);
     }
 
     @Test
      void secondTest(){
-        Assertions.assertFalse(3 < 1);
+        Assertions.assertTrue(3 > 1);
     }
 }
